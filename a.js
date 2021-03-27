41947c3218cb5d8d6741aa09efb59ec37fa0c174
Gaurav <gaurav.chillz@gmail.com>
2021-03-27 19:30:02 +0530

    Initial commit

---
+++ a.js
@@ -0,0 +1,5 @@
+// typing text animation script
+<button onclick="document.getElementById('name').value = ''">Clear input field</button>
+<button onclick="document.getElementById('email').value = ''">Clear input field</button>
+<button onclick="document.getElementById('phone').value = ''">Clear input field</button>
+<button onclick="document.getElementById('message').value = ''">Clear input field</button>
